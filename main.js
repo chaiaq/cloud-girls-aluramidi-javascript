0f1a3273fc755a5f4fa05dd90b54cf1b66c4bdba
chaiaq <chaiane.ey@gmail.com>
2023-10-04 21:06:38 -0300

    04/10

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio) {
+    document.querySelectorAll(idElementoAudio);
+    
+}
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+// irá gerar a lista de teclas captadas pelo querySelectorAll
+
+console.log(listaDeTeclas);
+
+let contador = 0;
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSom;
+    contador++;
+
+    console.log(contador);
+
+}
+
 
